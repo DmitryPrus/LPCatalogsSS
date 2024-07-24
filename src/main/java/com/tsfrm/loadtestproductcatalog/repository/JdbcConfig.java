e79e31318131e44bc205f346747337bd7236f9a1
DmitryPrus <dmitri.prus@365smartshop.com>
2024-07-24 17:50:03 +0300

    new update

--- src/main/java/com/tsfrm/loadtestproductcatalog/repository/JdbcConfig.java
+++ src/main/java/com/tsfrm/loadtestproductcatalog/repository/JdbcConfig.java
@@ -3,13 +3,13 @@ package com.tsfrm.loadtestproductcatalog.repository;
 public class JdbcConfig {
 
     // test 4
-    public String dbUrl = System.getenv("DB_URL") != null ? System.getenv("DB_URL") : "jdbc:mysql://test4db.365rm.us/sosdb";
-    public String dbUser = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : "v1vdiapi2SOSDB";
-    public String dbPassword = System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD") : "AQ5t6mZXGBFW";
+//    public String dbUrl = System.getenv("DB_URL") != null ? System.getenv("DB_URL") : "jdbc:mysql://test4db.365rm.us/sosdb";
+//    public String dbUser = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : "v1vdiapi2SOSDB";
+//    public String dbPassword = System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD") : "AQ5t6mZXGBFW";
 
     // staging
 
-//    public String dbUrl = System.getenv("DB_URL") != null ? System.getenv("DB_URL") : "jdbc:mysql://stagingdb.365rm.us/sosdb";
-//    public String dbUser = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : "v1vdiapi2SOSDB";
-//    public String dbPassword = System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD") : "S03LRm2PZhkU";
+    public String dbUrl = System.getenv("DB_URL") != null ? System.getenv("DB_URL") : "jdbc:mysql://stagingdb.365rm.us/sosdb";
+    public String dbUser = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : "v1vdiapi2SOSDB";
+    public String dbPassword = System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD") : "S03LRm2PZhkU";
 }
